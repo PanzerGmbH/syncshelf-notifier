6a08fc046caea9057eb7f352fb451655d1fa14a2
PanzerGmbH <168515505+PanzerGmbH@users.noreply.github.com>
2025-06-01 18:29:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -49,3 +49,11 @@ app.get("/", async (req, res) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server läuft auf Port ${PORT}`);
+});
+
+
+
